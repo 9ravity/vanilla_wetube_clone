817a2913cdb2f6df0d481235fd59ac481355882d
vinary-com <shegoback@gmail.com>
2019-10-14 20:59:30 +0900

    noh : babel-설치 및 setting

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-fgjgk;
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+function handleListening() {
+  console.log(`http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
