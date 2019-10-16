23f22f4321ebf63e9ddc2a5f65a9ec8733c33660
vinary-com <shegoback@gmail.com>
2019-10-16 21:15:01 +0900

    url 표시

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routers";
 
 const app = express();
 
@@ -11,5 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
