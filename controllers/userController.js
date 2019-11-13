d600db9350de731d12fe2fe18097458cfddcc654
vinary-com <shegoback@gmail.com>
2019-11-13 20:59:01 +0900

    locals, #{} 변수

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,13 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
+export const join = (req, res) => res.send("join", { pageTitle: "USER JOIN" });
+export const login = (req, res) =>
+  res.send("login", { pageTitle: "USER LOGIN" });
+export const logout = (req, res) =>
+  res.send("logout", { pageTitle: "USER LOGOUT" });
 
-export const users = (req, res) => res.send("users");
-export const userDetail = (req, res) => res.send("userDetail");
-export const editProfile = (req, res) => res.send("editProfile");
-export const changePassword = (req, res) => res.send("changePassword");
+export const users = (req, res) => res.send("users", { pageTitle: "USERS" });
+export const userDetail = (req, res) =>
+  res.send("userDetail", { pageTitle: "USER DETAIL" });
+export const editProfile = (req, res) =>
+  res.send("editProfile", { pageTitle: "EDIT PROFILE" });
+export const changePassword = (req, res) =>
+  res.send("changePassword", { pageTitle: "CHANGE PASSWORD" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,11 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("search");
-
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "HOME" });
+export const search = (req, res) => res.send("search", { pageTitle: "SEARCH" });
+export const videos = (req, res) => res.send("videos", { pageTitle: "VIDEOS" });
+export const upload = (req, res) =>
+  res.send("upload", { pageTitle: "UPLOAD VIDEO" });
+export const videoDetail = (req, res) =>
+  res.send("videoDetail", { pageTitle: "VIDEO DETAIL" });
+export const editVideo = (req, res) =>
+  res.send("editVideo", { pageTitle: "EDIT VIDEO" });
+export const deleteVideo = (req, res) =>
+  res.send("deleteVideo", { pageTitle: "DELETE VIDEO" });
