6f152eb5259fab98ffd16bf6e67af3917920e9fe
9ravity <shegoback@naver.com>
2020-03-12 20:00:00 +0900

    User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,26 @@
 /* eslint-disable no-console */
 import routers from "../routers";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "USER JOIN" });
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   console.log(`posjoin:${req.body}`);
-  const { name, eamil, password, password2 } = req.body;
+  const { name, email, password, password2 } = req.body;
   if (password !== password2) {
     res.status(400);
     res.render("join", { pageTitle: "USER JOIN" });
   } else {
     // todo : 사용자 등록 -> login
+
+    try {
+      const user = await User({ name, email });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
+
     res.redirect(routers.home);
   }
 };
